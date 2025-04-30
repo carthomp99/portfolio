09d4bc0eb9b25a17316bb8c2c0d3e8d1474391f3
Carter Thompson <carthomp88@gmail.com>
2025-04-30 16:43:55 -0500

    Portfolio sections

---
+++ portraits.js
@@ -0,0 +1,33 @@
+const gallery = document.getElementById("gallery")
+
+const displayGallery = (json) => {
+    json.forEach(piece => {
+        const image = document.createElement("img")
+        image.src=piece.localurl
+        image.alt= "" + piece.name + " " + piece.blurb
+
+        const imageBox = document.createElement("div")
+
+        const imageName = document.createElement("h5")
+        const imageBlurb = document.createElement("p")
+        const year = document.createElement("p")
+        imageName.innerHTML = piece.name
+        imageBlurb.innerHTML = piece.blurb
+        year.innerHTML = "" + piece.year
+
+        const textPortion = document.createElement("div")
+        textPortion.className = "img_description"
+        textPortion.appendChild(imageName)
+        textPortion.appendChild(imageBlurb)
+
+        imageBox.className = "img_wrap"
+        imageBox.appendChild(image)
+        imageBox.appendChild(textPortion)
+        gallery.appendChild(imageBox)
+    })
+}
+
+fetch('./portraits.json')
+    .then(response => response.json())
+    .then(json => displayGallery(json))
+
